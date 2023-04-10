4c5b583d9ad62e86787ad7f493b950c9a8cce9b0
jason <114672583+ddys001@users.noreply.github.com>
2023-04-10 13:51:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 echo "ZFile-On-Replit一键搭建脚本"
-echo "脚本作者：舒夏"
-echo "GitHub开源地址：https://github.com/sxbai/zfile-on-replit"
+echo "脚本作者：豆豆"
+echo "GitHub开源地址：https://github.com/ddys001/zfile-on-replit"
 nix-env -iA nixpkgs.wget
 mkdir build
 cd build
-wget -O .replit https://github.com/sxbai/zfile-on-replit/raw/master/.replit
-wget -O replit.nix https://github.com/sxbai/zfile-on-replit/raw/master/replit.nix
+wget -O .replit https://github.com/ddys001/zfile-on-replit/raw/main/.replit
+wget -O replit.nix https://github.com/ddys001/zfile-on-replit/raw/main/replit.nix
 cd ..
 wget -O main.sh https://github.com/sxbai/zfile-on-replit/raw/master/main.sh
 cp -r build/.replit . && cp -r build/replit.nix .
